c4bd5b85ea9c45adfbb6d850d3c3d97b59268e37
chantityb <chantityb@gmail.com>
2018-10-25 20:11:29 -0400

    initial commit

---
+++ src/Navigation.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+//import './App.css';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div>
+      	<nav>
+      		<h2>React Portfolio</h2>
+
+      		<ul>
+      			<li>Home</li>
+      			<li>About</li>
+      			<li>Contact</li>
+			</ul>
+
+      	</nav>
+       
+      </div>
+    );
+  }
+}
+
+export default Navigation;
