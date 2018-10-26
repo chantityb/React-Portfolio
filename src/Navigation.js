ac39959a8e19e1bf79fbbf5c78a5cee9f38b6aab
chantityb <chantityb@gmail.com>
2018-10-25 20:38:14 -0400

    Added Navigation component

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-//import './App.css';
+import './Navigation.css';
 
 class Navigation extends Component {
   render() {
     return (
       <div>
       	<nav>
-      		<h2>React Portfolio</h2>
+      		<h2 className="logo">React Portfolio</h2>
 
       		<ul>
-      			<li>Home</li>
-      			<li>About</li>
-      			<li>Contact</li>
+      			<li><a href="#">Home</a></li>
+      			<li><a href="#">About</a></li>
+      			<li><a href="#">Service</a></li>
+      			<li><a href="#">Portfolio</a></li>
+				<li><a href="#">Contact</a></li>
 			</ul>
 
       	</nav>
