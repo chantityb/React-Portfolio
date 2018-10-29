48384c37542f2d31f6fd91d76c1a2863db3631ce
chantityb <chantityb@gmail.com>
2018-10-29 00:37:14 -0400

    Added Header component along with styling

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Background from './img/bg-masthead.jpg';
+import './Header.css';
+
+const myStyles = {
+		backgroundImage: `url( ${Background} )`,
+		height: '80vh',
+		backgroundSize: 'cover'
+	}
+
+
+class Header extends Component {
+
+	render(){
+		return(
+			<header style={myStyles}>
+				<h1>{this.props.title}</h1>
+				<p>A Free Bootstrap Theme by Start Bootstrap</p>
+				<a href="#button">{this.props.button}</a>
+			</header>
+		)
+	}
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import Navigation from './Navigation';
+import Header from './Header';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Navigation />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+class App extends Component {
+	render(){
+		return(
+			
+			<div>
+				<Navigation LogoTitle="Chantity Buraimoh" />
+				<Header title="Stylish Portfolio" button="Find Out More"/>
+			</div>
+
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 serviceWorker.unregister();
